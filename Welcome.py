7725e9653589459183d81bcbc06caa7bcb091a56
Zachary Villarimo <02zachary.villarimo@gulllakecs.org>
2022-02-08 14:51:02 -0500

    Import time library(sleep)

---
+++ Welcome.py
@@ -0,0 +1,7 @@
+# Code Name - Hornet
+
+from time import sleep #print to one line with time delay between prints
+
+print("Welcome to Hornet InfoTechCenter\n") ; sleep(1)
+
+print("Hornet's Operating system booting up")
